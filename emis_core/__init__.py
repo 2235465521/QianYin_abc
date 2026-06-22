08c5c4eaa4c5ff0b71b1c41c1f7bbcc6f82fbece
lsj <2235465521@qq.com>
2026-06-22 18:05:16 +0800

    Replace mysql-client with PyMySQL and mock version to resolve Linux compilation issues

--- emis_core/__init__.py
+++ emis_core/__init__.py
@@ -1,5 +1,7 @@
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
+import pymysql
+pymysql.version_info = (2, 2, 2, 'final', 0)
+pymysql.install_as_MySQLdb()
+
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
